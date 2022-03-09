00e3998256340870704f75f6fd936c99e9f48134
Aleksei Kornienko <AD.Kornienko@yandex.ru>
2022-03-09 13:20:50 +0300

    Add homework №2

---
+++ src/Lesson_2/WriteToFile.java
@@ -0,0 +1,18 @@
+package Lesson_2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+public class WriteToFile {
+
+
+    void writeScore() throws FileNotFoundException {
+         File scoreTable = new File("C:\\Users\\User\\Desktop\\Y_lab\\src\\Lesson_2\\ResultTable");
+         PrintWriter printWriter = new PrintWriter(new FileOutputStream(scoreTable,true));
+         printWriter.println(Player1.getName() + " - " + " счёт: " + Player1.getScore());
+         printWriter.println(Player2.getName() + " - " + " счёт: " + Player2.getScore());
+         printWriter.close();
+    }
+}
